8c0b8e905f99542ce7374abc54597311267dbe6e
James <jpfairbanks@gmail.com>
2016-03-08 19:02:01 -0500

    Add Network type

--- src/Networks.jl
+++ src/Networks.jl
@@ -1,5 +1,20 @@
 module Networks
+using LightGraphs
 
-# package code goes here
+abstract AbstractNetwork
+
+"""Network{G,V,E}
+Network is the core type for store Graphs with vertex and edge properties.
+The vertex properites are of type V and the edge properties are of type E.
+"""
+type Network{G,V,E} <: AbstractNetwork
+  graph::G
+  vprop::Vector{V}
+  eprop::Dict{Edge,E}
+end
+
+import Base.==
+
+==(n::Network, m::Network) = (n.graph == m.graph) && (n.vprop == m.vprop) && (n.eprop == m.eprop)
 
 end # module
