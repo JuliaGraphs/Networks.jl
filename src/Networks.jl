e18e6526191ed1680aaf9b91ed745cb2b2545b2f
David Sanders <dpsanders@gmail.com>
2016-09-22 12:51:51 -0400

    Some clean-up and rewording

--- src/Networks.jl
+++ src/Networks.jl
@@ -1,32 +1,36 @@
 module Networks
-using LightGraphs
-import LightGraphs: Graph, DiGraph, fadj,
+
+import LightGraphs: Graph, DiGraph, SimpleGraph, Edge,
+        fadj,
         add_vertex!, add_vertices!, add_edge!
+
 import Base.convert, Base.promote_rule
+import Base.==
 
 export AbstractNetwork, Network
 abstract AbstractNetwork
 
-"""Network{G,V,E}
-Network is the core type for store Graphs with vertex and edge properties.
-The vertex properites are of type V and the edge properties are of type E.
+"""
+    `Network{G,V,E}`
+
+`Network` is the core type for representing graphs with vertex and edge properties.
+`G` is the type of the underlying graph; the vertex properties are of type `V`,
+and the edge properties are of type `E`.
 """
 type Network{G,V,E} <: AbstractNetwork
   graph::G
-  vprop::Vector{V}
-  eprop::Dict{Edge,E}
+  vprops::Vector{V}
+  eprops::Dict{Edge,E}
 end
 
+# convenience constructors for empty `Network`s with given types:
 Network{G,V,E}(::Type{G}, ::Type{V}, ::Type{E}) = Network(G(), V[], Dict{Edge,E}())
 Network{G,V}(::Type{G}, ::Type{V}) = Network(G(), V[], Dict{Edge,Void}())
 
 
-
-import Base.==
-
 function add_vertex!{G,V,E}(g::Network{G,V,E}, vertex_info::V)
     n = add_vertex!(g.graph)
-    push!(g.vprop, vertex_info)
+    push!(g.vprops, vertex_info)
 
     return n
 end
@@ -34,23 +38,25 @@ end
 function add_vertices!{G,V,E}(g::Network{G,V,E}, vertex_info::Vector{V})
     for vertex in vertex_info
         add_vertex!(g.graph)
-        push!(g.vprop, vertex)
+        push!(g.vprops, vertex)
     end
 end
 
 function add_edge!{G,V,E}(g::Network{G,V,E}, i, j, edge_info::E)
     e = add_edge!(g.graph, i, j)
-    (g.eprop)[e] = edge_info
+    (g.eprops)[e] = edge_info
     return e
 end
 
 add_edge!{G,V,E}(g::Network{G,V,E}, i, j) = add_edge!(g.graph, i, j)
 
 
+==(n::Network, m::Network) = (n.graph == m.graph) && (n.vprops == m.vprops) && (n.eprops == m.eprops)
 
-==(n::Network, m::Network) = (n.graph == m.graph) && (n.vprop == m.vprop) && (n.eprop == m.eprop)
+# conversion to underlying Graph:
 
 Graph(net::Network) = net.graph
+
 function convert{T<:SimpleGraph, G,V,E}(::Type{T}, net::Network{G,V,E})
     if typeof(net.graph) <: T
         return net.graph
